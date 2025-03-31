c0c78fce5a120a2e876689da76ff18a12a334d83
GAUTAM KUMAR PRAJAPATI <gautamkumarprajapati8@gmail.com>
2025-03-31 21:50:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (88.71%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
+class Solution2 {
+    public int strStr(String haystack, String needle) {
+        int hLen = haystack.length();
+        int nLen = needle.length();
+        
+        for (int i = 0; i <= hLen - nLen; i++) {
+            if (haystack.substring(i, i + nLen).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
