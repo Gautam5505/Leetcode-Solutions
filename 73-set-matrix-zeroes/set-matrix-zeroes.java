0ca298a3babea94f297f95e80c81bb72ed2deb0c
GAUTAM KUMAR PRAJAPATI <gautamkumarprajapati8@gmail.com>
2025-04-02 11:20:42 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (28.96%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void setZeroes(int[][] arr) {
+        int m = arr.length,n = arr[0].length;
+         boolean[] row = new boolean[m];
+         boolean[] col = new boolean[n];
+        //marking the particular roe and col
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(arr[i][j]==0){
+                    row[i]=true;
+                    col[j]=true;
+                }
+            }
+        }
+        //set the true row to 0
+        for(int i = 0; i < m; i++){
+            if(row[i]==true){
+                for(int j = 0; j < n; j++){
+                    arr[i][j]=0;
+                }
+            }
+        }
+        //set the true col 0
+        for(int j = 0; j < n; j++){
+            if(col[j]==true){
+                for(int i = 0; i < m; i++){
+                    arr[i][j]=0;
+                }
+            }
+        }
+    }
+}
