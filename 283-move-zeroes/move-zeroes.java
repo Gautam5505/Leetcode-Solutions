a6ab68ad59517028e9e5c6c77fe03655e243381a
GAUTAM KUMAR PRAJAPATI <gautamkumarprajapati8@gmail.com>
2025-04-08 12:08:38 +0530

    Time: 2 ms (84.95%) | Memory: 46.2 MB (32.09%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,18 +1,15 @@
-class Solution {
-    public void moveZeroes(int[] arr) {
-        int noz = 0;
-        int n = arr.length;
-        for(int ele : arr){
-            if(ele == 0)noz++;
-        }
-        for(int i = 0; i < noz; i++){
-            for(int j = 0; j < n - 1- i; j++){
-                if(arr[j] == 0){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;  
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                nums[i] = nums[j];  
+                if (i != j) {
+                    nums[j] = 0;  
                 }
+                i++;
             }
         }
     }
+   
 }
